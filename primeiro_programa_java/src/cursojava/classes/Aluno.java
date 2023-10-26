e0919b8847fe5401f5bf58ee105764e80054cbc0
Bruno Wellington <brunowellington9@gmail.com>
2023-10-25 22:00:24 -0300

    Primeiro commit: Entendendo os metodos get e set

---
+++ primeiro_programa_java/src/cursojava/classes/Aluno.java
@@ -0,0 +1,170 @@
+package cursojava.classes;
+
+public class Aluno {
+	/*Atributos*/
+	private String nome;
+	private int idade;
+	private String dataNascimento;
+	private String registroGeral;
+	private String numeroCpf;
+	private String nomeMae;
+	private String nomePai;
+	private String dataMatricula;
+	private String nomeEscola;
+	private String serieMatriculado;
+	private double nota1;
+	private double nota2;
+	private double nota3;
+	private double nota4;
+	
+	
+	public Aluno() { /* CRIA OS DADOS NA MEMORIA - SENDO PADRAO DO JAVA */
+		
+	}
+	
+	public Aluno(String nomePadrao){
+		nome = nomePadrao;
+	}
+	
+	public Aluno(String nomePadrao, int idadePadrao){
+		nome = nomePadrao;
+		idade = idadePadrao;
+	}
+	
+	/* Metodos GETTERS e SETTERS */ 
+	/* GET é para resgatar ou obter o valor do atributo */
+	/* SET é para adicionar ou receber dados para os atributos */
+	
+	
+	
+	/* GET/SET - Nome */
+	public String getNome(){
+		return nome;
+	}
+	public void setNome(String nome){
+		this.nome = nome; /* THIS é utilizado para referenciar ao atributo nome da classe Aluno*/
+	}
+	
+	
+	/* GET/SET - Idade */
+	public int getIdade() {
+		return idade;
+	}
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+	
+	
+	/* GET/SET - dataNascimento */
+	public String getDataNascimento() {
+		return dataNascimento;
+	}
+	public void setDataNascimento(String dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
+	
+	
+	/* GET/SET - registroGeral */
+	public String getRegistroGeral() {
+		return registroGeral;
+	}
+	public void setRegistroGeral(String registroGeral) {
+		this.registroGeral = registroGeral;
+	}
+	
+	
+	/* GET/SET - numeroCpf */
+	public String getNumeroCpf() {
+		return numeroCpf;
+	}
+	public void setNumeroCpf(String numeroCpf) {
+		this.numeroCpf = numeroCpf;
+	}
+	
+	
+	/* GET/SET - nomeMae */
+	public String getNomeMae() {
+		return nomeMae;
+	}
+	public void setNomeMae(String nomeMae) {
+		this.nomeMae = nomeMae;
+	}
+	
+	
+	/* GET/SET - nomePai */
+	public String getNomePai() {
+		return nomePai;
+	}
+	public void setNomePai(String nomePai) {
+		this.nomePai = nomePai;
+	}
+	
+	
+	/* GET/SET - dataMatricula */
+	public String getDataMatricula() {
+		return dataMatricula;
+	}
+	public void setDataMatricula(String dataMatricula) {
+		this.dataMatricula = dataMatricula;
+	}
+	
+	
+	/* GET/SET - nomeEscola */
+	public String getNomeEscola() {
+		return nomeEscola;
+	}
+	public void setNomeEscola(String nomeEscola) {
+		this.nomeEscola = nomeEscola;
+	}
+	
+	
+	/* GET/SET - serieMatriculado */
+	public String getSerieMatriculado() {
+		return serieMatriculado;
+	}
+	public void setSerieMatriculado(String serieMatriculado) {
+		this.serieMatriculado = serieMatriculado;
+	}
+
+	
+	/* GET/SET - nota1 */
+	public double getNota1() {
+		return nota1;
+	}
+	public void setNota1(double nota1) {
+		this.nota1 = nota1;
+	}
+
+	
+	/* GET/SET - nota2 */
+	public double getNota2() {
+		return nota2;
+	}
+	public void setNota2(double nota2) {
+		this.nota2 = nota2;
+	}
+
+	
+	/* GET/SET - nota3 */
+	public double getNota3() {
+		return nota3;
+	}
+	public void setNota3(double nota3) {
+		this.nota3 = nota3;
+	}
+
+	
+	/* GET/SET - nota4 */
+	public double getNota4() {
+		return nota4;
+	}
+	public void setNota4(double nota4) {
+		this.nota4 = nota4;
+	}
+	
+	/***********************************/
+	/* Metodo get para calcular e retornar a media*/
+	public double getMediaAluno() {
+		return (nota1 + nota2 + nota3 + nota4)/4;
+	}
+}
---
+++ primeiro_programa_java/src/cursojava/executavel/PrimeiraClasseJava.java
@@ -0,0 +1,63 @@
+package cursojava.executavel;
+
+import cursojava.classes.Aluno;
+
+public class PrimeiraClasseJava {
+	
+	public static void main(String[] args) {
+		
+		Aluno aluno0; /*objeto ainda nao existe na memoria*/
+		
+		/*new Aluno() é uma instancia (criação de objeto)*/
+		/*O aluno1,2,3... é uma referencia para o objeto aluno*/
+		
+		Aluno aluno1 = new Aluno(); /*Objeto criado*/
+		
+		aluno1.setNome("João da Silva");
+		aluno1.setIdade(50);
+		aluno1.setDataNascimento("18/10/1987");
+		aluno1.setRegistroGeral("44554.4544.545");
+		aluno1.setNumeroCpf("536.265.774-89");
+		aluno1.setNomeMae("Shirlei da Silva");
+		aluno1.setNomePai ("Jose da Silva");
+		aluno1.setDataMatricula("10/01/2019");
+		aluno1.setNomeEscola("Santos Dumont");
+		aluno1.setSerieMatriculado("5");
+		aluno1.setNota1(90);
+		aluno1.setNota2(80.8);
+		aluno1.setNota3(70.9);
+		aluno1.setNota4(90.7);
+		
+		
+		System.out.println("Nome do aluno: " + aluno1.getNome());
+		System.out.println("Idade: " + aluno1.getIdade() + " Anos");
+		System.out.println("Data de Nascimento: " + aluno1.getDataNascimento());
+		System.out.println("Media do Aluno: " + aluno1.getMediaAluno() + "\n");
+		
+		/*============================================================================*/
+		
+		
+		Aluno aluno2 = new Aluno(); /*Objeto criado*/
+		aluno2.setNome("Antonio");
+		aluno2.setIdade(25);
+		aluno2.setDataNascimento("12/05/2003");
+		aluno2.setNota1(70);
+		aluno2.setNota2(68.8);
+		aluno2.setNota3(56.9);
+		aluno2.setNota4(88.7);
+		
+		System.out.println("Nome do aluno: " + aluno2.getNome());
+		System.out.println("Idade: " + aluno2.getIdade() + " Anos");
+		System.out.println("Data de Nascimento: " + aluno2.getDataNascimento());
+		System.out.println("Media do Aluno: " + aluno2.getMediaAluno() + "\n");
+		
+		/*============================================================================*/
+		
+		
+		Aluno aluno3 = new Aluno(); /*Objeto criado*/
+		
+		Aluno aluno4 = new Aluno();
+		Aluno aluno5 = new Aluno();
+		
+	}
+}
